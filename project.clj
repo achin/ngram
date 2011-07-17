6cef9ada4cb9a6537e6a0c23db7f378eef3943df
Alex Chin <achin@texeltek.com>
2011-07-17 16:02:16 -0400

    Added midje.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject ngram "1.0.0-SNAPSHOT"
   :description "Simple Clojure Hadoop project"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [clojure-hadoop/clojure-hadoop "1.3.1-SNAPSHOT"]])
+                 [clojure-hadoop/clojure-hadoop "1.3.1-SNAPSHOT"]]
+  :dev-dependencies [[midje "1.1.1"]])
