ddc0dc140282b02c90603cd91d1485c69cb9d8cc
Alex Chin <achin@texeltek.com>
2011-07-17 16:01:06 -0400

    Added clojure-hadoop dependency.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject ngram "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :description "Simple Clojure Hadoop project"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [clojure-hadoop/clojure-hadoop "1.3.1-SNAPSHOT"]])
