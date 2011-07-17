1dc6cd95c785d1fb7f27ecdac149a8bd7017878a
Alex Chin <achin@texeltek.com>
2011-07-17 16:17:45 -0400

    Added tests and impl for ngrams

--- src/ngram/core.clj
+++ src/ngram/core.clj
@@ -1,1 +1,8 @@
-(ns ngram.core)
+(ns ngram.core
+  (:require [clojure.contrib.str-utils2 :as str2]))
+
+(defn ngrams [n s]
+  (when (> n 0)
+    (let [padding (str2/repeat " " (- n 1))]
+      (map #(apply str %)
+           (partition n 1 (str padding s padding))))))
--- test/ngram/test/core.clj
+++ test/ngram/test/core.clj
@@ -1,3 +1,11 @@
 (ns ngram.test.core
   (:use [ngram.core])
   (:use [midje.sweet]))
+
+(fact "ngrams can generate basic values"
+      (ngrams 1 "abc") => '("a" "b" "c")
+      (ngrams 2 "abc") => '(" a" "ab" "bc" "c ")
+      (ngrams 3 "abcde") => '("  a" " ab" "abc" "bcd" "cde" "de " "e  "))
+
+(fact "ngrams doesn't do anything if n is zero"
+      (ngrams 0 ...anything...) => nil)
