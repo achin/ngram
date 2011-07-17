e47e1d18395e4cbfcb991bcd88ba0849d744ede3
Alex Chin <achin@texeltek.com>
2011-07-17 16:42:44 -0400

    Added my-map and my-reduce.

--- src/ngram/core.clj
+++ src/ngram/core.clj
@@ -6,3 +6,11 @@
     (let [padding (str2/repeat " " (- n 1))]
       (map #(apply str %)
            (partition n 1 (str padding s padding))))))
+
+(defn my-map [key value]
+  (map list
+       (ngrams 3 value)
+       (repeat 1)))
+
+(defn my-reduce [key value-fn]
+  (list (list key (reduce + (value-fn)))))
