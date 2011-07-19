98e1498b36b4c62e0a4b8d15ffc8f70750402538
Alex Chin <achin@texeltek.com>
2011-07-18 20:36:20 -0400

    Fixed up Hadoop job readers and writers to use new mapreduce API. Job works.

--- src/ngram/hadoop.clj
+++ src/ngram/hadoop.clj
@@ -7,13 +7,22 @@
 (imp/import-io)
 (imp/import-mapreduce)
 
+; these readers and writers need tests!
+(defn string-long-reduce-reader [^Text key wvalues]
+  [(.toString key)
+   (fn [] (map (fn [^LongWritable v] (.get v))
+               (seq wvalues)))])
+
+(defn string-long-writer [^TaskInputOutputContext context key value]
+  (.write context (Text. key) (LongWritable. value)))
+
 (defjob/defjob job
   :map my-map
   :map-reader wrap/int-string-map-reader
-  :map-writer nil
+  :map-writer string-long-writer
   :reduce my-reduce
-  :reduce-reader nil
-  :reduce-writer nil
+  :reduce-reader string-long-reduce-reader
+  :reduce-writer string-long-writer
   :output-key Text
   :output-value LongWritable
   :input-format :text
